bec357e37f173893fdf4c59d1b0a15b9dfe04f92
Julia Wasala <34676810+BattleBunny@users.noreply.github.com>
2018-12-09 23:02:14 +0100

    Update 6.py

--- 6.py
+++ 6.py
@@ -4,12 +4,10 @@ lines = f.readlines()
 coordinates = [re.findall("[\w']+", line) for line in lines] 
 x = int(max(coordinates, key=lambda x:int(x[0]))[0])+1
 y = int(max(coordinates, key=lambda x:int(x[1]))[1])+1
-minx = int(min(coordinates, key=lambda x:int(x[0]))[0])
-miny = int(min(coordinates, key=lambda x:int(x[1]))[1])
-locations = [[[500,0] for j in range(x-minx)] for i in range(y-miny)]
+locations = [[[500,0] for j in range(x)] for i in range(y)]
 
-for i in range(y-miny):
-    for j in range(x-minx):
+for i in range(y):
+    for j in range(x):
         for k in range(len(lines)):
             distance = abs(i-int(coordinates[k][0])) + abs(j-int(coordinates[k][1]))
             if distance == locations[i][j][0]:
@@ -19,13 +17,13 @@ for i in range(y-miny):
                 locations[i][j][1] = k
 scores = [0]*len(lines)
 
-for i in range(y-miny):
-    for j in range(x-minx):
+for i in range(y):
+    for j in range(x):
         if locations[i][j][0] != 0:
-            if (i == 0 or j == 0 or i == y-miny or j == x-minx):
+            if (i == 0 or j == 0 or i == y-1 or j == x-1):
                 scores[locations[i][j][1]] = -1
             elif scores[locations[i][j][1]] != -1:
                 scores[locations[i][j][1]] += 1
-
+            
 print(sorted(scores))
 print(max(scores))
